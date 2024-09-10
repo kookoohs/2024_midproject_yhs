b0dd2bf227a8b02668a82057f4c0e70ad6a29d78
USERNAME <kookoo0@hs.ac.kr>
2024-09-10 07:56:05 +0000

    pentagon test push

--- Backend/utils/setup_db.js
+++ Backend/utils/setup_db.js
@@ -8,11 +8,11 @@ const setup = async () => {
 
     try {
         mysqldb = mysql.createConnection({
-            host: process.env.MYSQL_HOST,
-            port: process.env.MYSQL_PORT,
-            database: process.env.MYSQL_DATABASE,
-            user: process.env.MYSQL_USER,
-            password: process.env.MYSQL_PASSWORD,
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            database: process.env.DB_NAME,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
         });
         mysqldb.connect();
         // console.log("MySQL 접속 성공.");
